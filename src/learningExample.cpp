cd0e50197a1c1d2bd7a3bbef0768e7d9f6435a50
Pierre-Yves <plerolla@insa-rennes.fr>
2020-07-02 10:37:53 +0200

    adding a logger and a validation phase to the learning process

--- src/learningExample.cpp
+++ src/learningExample.cpp
@@ -13,10 +13,6 @@
 #include "ALEWrapper.h"
 #include "replay.h"
 
-#ifndef NB_GENERATIONS
-#define NB_GENERATIONS 2000
-#endif
-
 int main() {
     // Create the instruction set for programs
     Instructions::Set set;
@@ -51,44 +47,19 @@ int main() {
     Learn::ParallelLearningAgent la(le, set, params);
     la.init();
 
+    Log::LABasicLogger l(std::cout);
+    la.addLogger(l);
+
 
     // Create an exporter for all graphs
     File::TPGGraphDotExporter dotExporter("out_000.dot", la.getTPGGraph());
-
-    auto start = std::chrono::system_clock::now();
     // Train for NB_GENERATIONS generations
-    printf("Gen\tNbVert\tMin\tAvg\tMax\tDuration(eval)\tDuration(training)\tTotal_time\n");
-    for (int i = 0; i < NB_GENERATIONS; i++) {
-        auto evalBegin = std::chrono::system_clock::now();
-
-
+    for (int i = 0; i < params.nbGenerations; i++) {
         char buff[16];
         sprintf(buff, "out_%03d.dot", i);
         dotExporter.setNewFilePath(buff);
         dotExporter.print();
-        std::multimap<std::shared_ptr<Learn::EvaluationResult>, const TPG::TPGVertex *> result;
-        result = la.evaluateAllRoots(i, Learn::LearningMode::VALIDATION);
-        auto iter = result.begin();
-        double min = iter->first->getResult();
-        std::advance(iter, result.size() - 1);
-        double max = iter->first->getResult();
-        double avg = std::accumulate(result.begin(), result.end(), 0.0,
-                                     [](double acc,
-                                        std::pair<std::shared_ptr<Learn::EvaluationResult>, const TPG::TPGVertex *> pair) -> double {
-                                         return acc + pair.first->getResult();
-                                     });
-        avg /= result.size();
-
-        auto evalEnd = std::chrono::system_clock::now();
-        double tEval = ((std::chrono::duration<double>) (evalEnd - evalBegin)).count();
-
         la.trainOneGeneration(i);
-
-        double tTraining = ((std::chrono::duration<double>) (std::chrono::system_clock::now() - evalEnd)).count();
-        double tTotal = ((std::chrono::duration<double>) (std::chrono::system_clock::now() - start)).count();
-        printf("%3d\t%4" PRIu64 "\t%1.2lf\t%1.2lf\t%1.2lf\t%1.2lf\t%1.2lf\t%1.2lf\n", i,
-               la.getTPGGraph().getNbVertices(), min, avg,
-               max, tEval, tTraining, tTotal);
     }
 
     // Keep best policy
