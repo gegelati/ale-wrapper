b7ac87a9d1f93a504f1ae5aab5a768e3cbedd38b
kdesnos <kdesnos@insa-rennes.fr>
2020-09-09 12:48:28 +0200

    (Scripts) Script scalability experiment.

--- scripts/experiments.sh
+++ scripts/experiments.sh
@@ -2,21 +2,37 @@
 
 declare -a games=("alien"  "asteroids" "centipede"  "fishing_derby"  "frostbite")
 
+nbThreads=`seq 2 24`
+
+seeds=`seq 0 4`
+seedsInParallel=false
+
 for game in "${games[@]}"; do
     echo "Training on $game"
 
-    # Start games
-    for i in `seq 0 4`; do
-        ../build/Release/ALEGegelati -s $i -r $game 2> /dev/null &
-        pids[${i}]=$!
+    for threads in ${nbThreads[@]}; do
+        echo "Nb thread $threads"
+
+        # Start games
+        if $seedsInParallel; then
+            echo "Launch games in parallel"
+            for i in ${seeds[*]}; do
+                 ../build/Release/ALEGegelati -s $i -r $game -p $threads 2> /dev/null &
+                 pids[${i}]=$!
+            done
+
+            # Wait for completion
+            for pid in ${pids[*]}; do
+                wait $pid
+            done
+        else
+            echo "Launch games sequentially"
+            for i in ${seeds[*]}; do
+                ../build/Release/ALEGegelati -s $i -r $game -p $threads 2> /dev/null
+            done
+
+        fi
     done
-
-    # Wait for completion
-    for pid in ${pids[*]}; do
-        wait $pid
-    done
-
-
 done
 
 echo Done
--- src/learningExample.cpp
+++ src/learningExample.cpp
@@ -5,6 +5,7 @@
 #include <cfloat>
 #include <inttypes.h>
 #include <getopt.h>
+#include <thread>
 
 #include <chrono>
 #include <ctime>
@@ -19,16 +20,19 @@ int main(int argc, char ** argv) {
     char option;
     uint64_t seed = 0;
     char rom[50];
+    size_t nbThreads = std::thread::hardware_concurrency();
     strcpy(rom, "frostbite");
-    while((option = getopt(argc, argv, "s:r:")) != -1){
+    while((option = getopt(argc, argv, "s:r:p:")) != -1){
         switch (option) {
             case 's': seed= atoi(optarg); break;
             case 'r': strcpy(rom, optarg); break;
-            default: std::cout << "Unrecognised option. Valid option is \'-s seed\'." << std::endl; exit(1);
+            case 'p': nbThreads = atoi(optarg); break;
+            default: std::cout << "Unrecognised option. Valid options are \'-s seed\' \'-r romName\' \'-p nbThreads\'." << std::endl; exit(1);
         }
     }
     std::cout << "Selected seed : " << seed << std::endl;
     std::cout << "Selected ROM: "  << rom << std::endl;
+    std::cout << "Selected NbThreads: " << nbThreads;
 
     // Create the instruction set for programs
     Instructions::Set set;
@@ -55,6 +59,7 @@ int main(int argc, char ** argv) {
     // among other things)
     Learn::LearningParameters params;
     File::ParametersParser::loadParametersFromJson(ROOT_DIR "/params-kelly.json",params);
+    params.nbThreads = nbThreads;
 
     char romPath[50];
     sprintf(romPath, ROOT_DIR "/roms/%s", rom);
@@ -67,25 +72,25 @@ int main(int argc, char ** argv) {
 
     // Basic Logger
     char logPath[50];
-    sprintf(logPath, "out.%s.%d.std", rom, seed);
+    sprintf(logPath, "out.%s.%d.t%02d.std", rom, seed, nbThreads);
     std::ofstream logStream;
     logStream.open(logPath);
     Log::LABasicLogger log(la, logStream);
 
     // File for logging best policy stat.
     char bestPolicyStatsPath[50];
-    sprintf(bestPolicyStatsPath, "bestPolicyStats.%s.%d.md", rom, seed);
+    sprintf(bestPolicyStatsPath, "bestPolicyStats.%s.%d.t%02d.md", rom, seed, nbThreads);
     std::ofstream stats;
     stats.open(bestPolicyStatsPath);
     Log::LAPolicyStatsLogger logStats(la, stats);
 
     // Create an exporter for all graphs
     char dotPath[50];
-    sprintf(dotPath, "out_0000.%s.%d.dot", rom, seed);
+    sprintf(dotPath, "out_0000.%s.%d.t%02d.dot", rom, seed, nbThreads);
     File::TPGGraphDotExporter dotExporter(dotPath, la.getTPGGraph());
     // Train for NB_GENERATIONS generations
     for (int i = 0; i < params.nbGenerations; i++) {
-        sprintf(dotPath, "out_%04d.%s.%d.dot", i, rom, seed);
+        sprintf(dotPath, "out_%04d.%s.%d.t%02d.dot", i, rom, seed, nbThreads);
         dotExporter.setNewFilePath(dotPath);
         dotExporter.print();
         la.trainOneGeneration(i);
@@ -94,7 +99,7 @@ int main(int argc, char ** argv) {
     // Keep best policy
     la.keepBestPolicy();
     char bestDot[50];
-    sprintf(bestDot, "out_best.%s.dot", rom);
+    sprintf(bestDot, "out_best.%s.%d.t%20d.dot", rom, seed, nbThreads);
     dotExporter.setNewFilePath(bestDot);
     dotExporter.print();
 
